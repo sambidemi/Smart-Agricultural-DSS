635836a7cf6b4944c593ab7aaaf6bda9e61456e1
sambidemi <owolabisamuel085@gmail.com>
2026-06-22 17:19:03 +0100

    updated the backend

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://neondb_owner:npg_vjJTHyL5pW6t@ep-fragrant-star-ab0na5gx-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require"
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_vjJTHyL5pW6t@ep-fragrant-star-ab0na5gx-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require",
+)
 
 engine = create_engine(
     DATABASE_URL,
